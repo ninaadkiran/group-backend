52acea56cf464e771882ec03e15090c10a9c9b36
daniel-s-lee <daniell20249@stu.powayusd.com>
2024-03-12 09:41:57 -0700

    basically nothing

--- api/user.py
+++ api/user.py
@@ -3,7 +3,6 @@ from flask import Blueprint, request, jsonify, current_app, Response
 from flask_restful import Api, Resource # used for REST API building
 from datetime import datetime
 from auth_middleware import token_required
-
 from model.users import User
 
 user_api = Blueprint('user_api', __name__,
